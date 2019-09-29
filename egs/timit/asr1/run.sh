0a2e51e02a3b02227d87f1fccbcd31274a09b1ab
angshuman <angshuman.das@iiitb.org>
2019-09-29 12:32:23 +0530

    spectrogram-1-check

--- egs/timit/asr1/run.sh
+++ egs/timit/asr1/run.sh
@@ -60,9 +60,9 @@ if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
     ### But you can utilize Kaldi recipes in most cases
     echo "stage 1: Feature Generation"
     fbankdir=fbank
-    # Generate the fbank features; by default 80-dimensional fbanks with pitch on each frame
+    # Generate the --spectrogram/stft-- features; by default 80-dimensional fbanks with pitch on each frame
     for x in test train dev; do
-        steps/make_fbank_pitch.sh --cmd "$train_cmd" --nj 8 --write_utt2num_frames true \
+        steps/make_spectrogram.sh --cmd "$train_cmd" --nj 8 --write_utt2num_frames true \
         data/${x} exp/make_fbank/${x} ${fbankdir}
     done
 
