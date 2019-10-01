47a4cb4df4eb0edbc56430edbc1090cf9d6aac8b
angshuman <angshuman.das@iiitb.org>
2019-10-02 00:04:43 +0530

    mfilter w/ delta code changes

--- egs/timit/asr1/run.sh
+++ egs/timit/asr1/run.sh
@@ -18,7 +18,7 @@ verbose=1      # verbose option
 resume=        # Resume the training from snapshot
 
 # feature configuration
-do_delta=false
+do_delta=true
 
 train_config=conf/train.yaml
 decode_config=conf/decode.yaml
@@ -62,7 +62,7 @@ if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
     fbankdir=fbank
     # Generate the fbank features; by default 80-dimensional fbanks with pitch on each frame
     for x in test train dev; do
-        steps/make_fbank_pitch.sh --cmd "$train_cmd" --nj 8 --write_utt2num_frames true \
+        steps/make_fbank.sh --cmd "$train_cmd" --nj 8 --write_utt2num_frames true \
         data/${x} exp/make_fbank/${x} ${fbankdir}
     done
 
